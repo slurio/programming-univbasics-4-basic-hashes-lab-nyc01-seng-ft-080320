fd8f0125b18df6386f18855be458201672f432d3
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-08 14:54:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,18 +3,14 @@ def new_hash
 end
 
 def my_hash
-  {:key => "value"}
+  { :key => "value" }
 end
 
 def pioneer
-  pioneer_hash = {
-    :name => "Grace Hopper"
-  }
+  { :name => "Grace Hopper" }
 end
 
 
 def id_hash_generator(number)
-  {
-    :id => number
-  }
+  { :id => number }
 end
